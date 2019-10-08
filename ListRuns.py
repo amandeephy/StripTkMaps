de17f1c31461e17b7c70ddaea0261dce2c419744
Guillermo A. Fidalgo-Rodríguez <guillermo.fidalgo@upr.edu>
2019-10-08 11:27:13 -0400

    Add files via upload

---
+++ ListRuns.py
@@ -0,0 +1,194 @@
+#!/usr/bin/python
+
+import __future__
+import codecs, os, re, subprocess,shlex,numpy
+import cernrequests,runregistry
+from bs4 import BeautifulSoup
+
+def getlist(file="ZeroBias_runs.txt"):
+    f=open(file).read()
+    runs= re.findall("000(\d{6})",str(f)) #get all 6 digit numbers followed by 000
+    intruns=[int(num) for num in runs] # make the list full on intergers
+    intruns.sort()  #have an ascending sorted list of runs 
+    if len(intruns)==0: # Check if list is empty
+        print("no runs found")
+    else:
+        print("Found some runs")
+    intruns=list(dict.fromkeys(intruns)) #make a list into a dict and the back into a list. This eliminates any duplicate runs
+    i=0
+    checkiter=[]
+    for x in intruns:    # Check if all runs are sorted from oldest to newest
+        if x==intruns[-1]:
+            break
+        elif x > intruns[i+1]:
+            print("iteration",i,"is greater the the next iteration")
+            checkiter.append(i)
+        i=i+1
+    repeat=getrepeatedruns(intruns)
+    
+    if len(checkiter)!=0:
+        return intruns,checkiter
+    else:
+        return intruns
+
+def getruntype_eos(runtype="ZeroBias"):
+    command="ls -R /eos/cms/store/group/comm_dqm/DQMGUI_data/ | grep "+runtype+" > "+runtype+"_runs.txt"
+    file=subprocess.call(command,shell=True)
+
+
+
+def getRR(first=272000,last=326000,tkr_IN=True,tkr_strip_ON=True,tkr_pix_ON=True,collisions=True):
+    runs = runregistry.get_runs(filter={
+        'run_number': { 'and': [{'>': first}, {'<': last}]},
+        'tracker_included': tkr_IN,
+        'tracker-strip': tkr_strip_ON,
+        'tracker-pixel': tkr_pix_ON
+        })
+    if collisions== True:
+        coltype=[]
+        for i in range(len(runs)):
+            coltype.append(re.findall(r'collision*',str(runs[i])))
+        print(coltype)
+        
+    return runs
+
+
+
+
+def getrepeatedruns(global_runs_with_rootfiles):
+    i=0
+    repeatruns=[]
+    for run in global_runs_with_rootfiles: 
+        if run == global_runs_with_rootfiles[-1]:
+    #         print "Last run",run
+            break
+        if run==global_runs_with_rootfiles[i+1]:
+            #print "repeated run:",run
+            repeatruns.append(run)
+        i+=1
+    print('there are',len(repeatruns),'repeated runs')
+    return repeatruns
+
+
+
+
+def compare_with_Gjsn(global_runs_with_rootfiles):
+    baseurl='https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/'
+    json_runs_2018="Collisions18/13TeV/ReReco/Cert_314472-325175_13TeV_17SeptEarlyReReco2018ABC_PromptEraD_Collisions18_JSON.txt"
+    json_runs_2017="Collisions17/13TeV/ReReco/Cert_294927-306462_13TeV_EOY2017ReReco_Collisions17_JSON_v1.txt"
+    json_runs_2016="Collisions16/13TeV/ReReco/Final/Cert_271036-284044_13TeV_ReReco_07Aug2017_Collisions16_JSON.txt"
+
+
+    json_runs=[json_runs_2018,json_runs_2017,json_runs_2016]#,url_runs_2015]
+    print('\n',"Fetching from: "+baseurl)
+    print('\n',"list of relative urls: ",json_runs)
+    
+    
+    json_runslist=[]
+    for url in json_runs:
+        response=cernrequests.get(baseurl+url) # open the document
+        index = response.text                  # read the document
+        a=str(index)                           # convert to string
+        soup = BeautifulSoup(a, 'html.parser') # create the soup object
+
+        a=re.findall(r"(\d{6})",str(soup))     #find exactly 6 occurrences of a number
+                                               #in other words find 6 digit numbers
+
+        print('\n',"amount of runs in the json",len(a),"in",url)
+        print('\n',"From",a[0],"to",a[-1])
+        json_runslist.append(a)
+    json_list = [item for sublist in json_runslist for item in sublist]
+
+    good=[]
+    missing=[]
+    bad=[]
+    for jrun in json_list:
+        for grun in global_runs_with_rootfiles:
+            if jrun==grun:
+                good.append(jrun)            
+        if jrun not in global_runs_with_rootfiles:
+            missing.append(jrun)
+
+    for grun in global_runs_with_rootfiles:
+        if grun not in json_list:
+            bad.append(grun)
+    # print "all good runs found",good,"\n"
+    print( '\n',len(good),"good runs")
+    print( '\n',len(missing),"missing runs")
+    print( '\n',len(bad),"bad runs")
+    return json_list,good,missing,bad
+
+
+
+
+
+
+def getruns_afs(year="all",rdirs='False'):
+
+    baseurl='https://cmsweb.cern.ch/dqm/offline/data/browse/ROOT/OfflineData/'
+    url_runs_2018="Run2018/ZeroBias/"
+    url_runs_2017="Run2017/ZeroBias/"
+    url_runs_2016="Run2016/ZeroBias/"
+    # url_runs_2015="Run2015/ZeroBias/"
+
+
+    url_runs=[url_runs_2018,url_runs_2017,url_runs_2016]#,url_runs_2015]
+    print("Fetching from: "+baseurl)
+    print("list of relative urls: ",url_runs)
+
+    global_rundir_with_rootfiles=[]
+    global_runs_with_rootfiles=[]
+    global_rundir_without_rootfiles=[]
+
+    # runs_without_rootfiles=[]
+    for url in url_runs:
+        response=cernrequests.get(baseurl+url) # open the document
+        index = response.text                  # read the document
+        a=str(index)                           # convert to string
+        soup = BeautifulSoup(a, 'html.parser') # create the soup object
+        RUNSXXRE= soup.body.find_all(string=re.compile("000.")) # the "." for a regular expresions means that it will expect ANY character EXCEPT newlines
+
+        rundir_with_rootfiles=[]
+        runs_with_rootfiles=[]
+        rundir_without_rootfiles=[]
+
+
+        for rundir in RUNSXXRE:
+            response=cernrequests.get(baseurl+url+rundir) # open the document
+            index = response.text                  # read the document
+            a=str(index)                           # convert to string
+            soup = BeautifulSoup(a, 'html.parser') # create the soup object
+            entries= soup.body.find_all(string=re.compile("DQM."))
+            if len(entries)==0:
+                rundir_without_rootfiles.append(str(rundir))        # Fill the list
+                global_rundir_without_rootfiles.append(str(rundir)) # Fill the list
+                #print rundir,"is empty"
+            else:
+
+                for n in re.findall(r"R(\d+)",str(entries)):    # This will only keep the 6 digits of the run numbers that we need (without the "R000")
+                    x=str(int(n))                               # Since we wont want to do math with these numbers I will convert them to strings
+                    runs_with_rootfiles.append(x)               # Fill the list
+                    global_runs_with_rootfiles.append(x)        # Fill the list
+
+                rundir_with_rootfiles.append(str(rundir))         # Fill the list
+                global_rundir_with_rootfiles.append(str(rundir))  # Fill the list
+
+                #print rundir,"has",len(entries),"root files"    
+        print("===========================================================")
+        print("For",url)
+        print("Out of a total of",len(RUNSXXRE),"run directories:\n",len(rundir_with_rootfiles),"have root files\n",len(rundir_without_rootfiles),"are empty")
+        print("There are",len(runs_with_rootfiles),"runs with root files")
+        print("\n")
+        #print "List of available runs for this year\n\n",runs_with_rootfiles
+
+    print("===========================================================")
+    print("For GLOBAL")
+    print("Out of all run directories:\n",len(global_rundir_with_rootfiles),"have root files\n",len(global_rundir_without_rootfiles),"are empty")
+    print("There are",len(global_runs_with_rootfiles),"runs with root files")
+    print("\n")
+    if rdirs=='True':
+        return global_runs_with_rootfiles,global_rundir_with_rootfiles,global_rundir_without_rootfiles
+    else:
+        return global_runs_with_rootfiles
+
+
--- README.md
+++ README.md
@@ -1,2 +1,147 @@
-# StripTkMaps
-A first repository to collect the restructuring of (strips) TkMaps related scripts for Tracker Offline shifters
+# ML4TRKDQM
+
+## Prerequisites
+
+This script was tested using ```Python``` version ```3.6```. 
+Although other versions might work too, this is the only one tested.
+
+Test your python version by running
+
+```bash
+python --version
+```
+
+```
+Python 3.6.8
+```
+## Setup
+
+At your terminal (choose different port for your case)
+
+#### At lpc
+```bash
+kinit username@FNAL.GOV
+
+ssh -L 127.0.0.1:9999:127.0.0.1:9999 username@cmslpc-sl6.fnal.gov
+cd path/to/your/working/area
+```
+#### Or at lxplus
+
+```bash
+
+ssh -L 127.0.0.1:9999:127.0.0.1:9999 username@lxplus.cern.ch
+cd path/to/your/working/area
+```
+
+You can choose a CMSSW release (but the script has only been tested on release 10_2_10)
+```bash
+cmsrel CMSSW_10_2_10
+cd CMSSW_10_2_10/src/
+cmsenv
+
+git clone git@github.com:GuillermoFidalgo/ML4TRKDQM.git
+#or 
+git clone https://github.com/GuillermoFidalgo/ML4TRKDQM.git
+
+cd ML4TRKDQM
+
+jupyter notebook --port 9999 --ip 127.0.0.1 --no-browser
+```
+**Check your terminal and copy paste URL into your browser**
+
+
+
+
+
+### In order to use the notebook you must have the appropiate certificate in the same directory as the script (Unless you specify the path to it)
+[Open the notebook](ListOfRuns.ipynb) to interact with the code and make the apporpiate changes to your needs.
+
+Notice that there is an `index.html` document. You should ignore this for now as we will create this file repeatedly and rewrite it in the script. This will not be needed in the script but will used for now in the python notebook.
+
+**After playing with the notebook you can see other functionality in the [script](ListRuns.py)**
+
+
+*Side Note* To use the script please look at [Authentication Setup](https://github.com/GuillermoFidalgo/ML4TRKDQM#authentication-setup)
+### Virtual environment
+
+*Side Note* **If you clone from this repo please remove `venv/` and all of its contents *before* creating a virtual enviroment**
+
+I highly recommend setting up a virtual environment to not get conflicts between different versions of packages.
+
+You can create a virtual python virtual environment with:
+
+```bash
+python3 -m venv venv
+```
+
+Then you need to *activate* the virtual environment
+
+```bash
+. venv/bin/activate
+```
+
+You should now see a ```(venv) ``` in front of you terminal line.
+
+```
+(venv) [yourname@yourmachine ML4TKRDQM]$
+```
+
+
+### Installing packages
+
+All tools necessary for data retrieval and data analysis are installable via ```pip```
+
+*pip* comes bundled in Python 3.6 so, you should already have it installed.
+
+These commands install all the tools necessary for this notebook. Each of them have their own documentation. You can start with the general documentation for most of the packages [here](https://github.com/ptrstn/cms-tracker-studies-notebook) and a <ins>seperate documentation</ins> for `runregistry` [here](https://github.com/fabioespinosa/runregistry_api_client) 
+
+```bash
+pip install git+https://github.com/CMSTrackerDPG/cernrequests
+pip install git+https://github.com/CMSTrackerDPG/runregcrawlr
+pip install git+https://github.com/CMSTrackerDPG/dqmcrawlr
+pip install git+https://github.com/CMSTrackerDPG/wbmcrawlr
+pip install git+https://github.com/CMSTrackerDPG/cms-tracker-studies
+pip install runregistry
+pip install numpy
+pip install matplotlib
+pip install pandas
+pip install seaborn
+pip install scipy
+pip install scikit-learn
+pip install bs4
+```
+
+*Side Note*: ```cernrequests``` is also available in pypi, so you could also install it via:
+
+```bash
+pip install cernrequests
+```
+
+This would install the current stable version of the package, 
+while the github link always installs the latest version inside the master branch of the repository.
+In order to update the pypi package, a maintainer is necessary which will leave in April 2019.
+
+
+## Authentication Setup
+
+A lot of resources at CERN are only accessible with proper authentication. 
+Some only require you to be within the CERN GPN, some require you to just authenticate with your Grid User Certificate, while others require cookies.
+In general should [request a grid user certificate](https://ca.cern.ch/ca/), if you dont already have.
+After that, you should have downloaded a file called ```myCertificate.p12```
+Now this file needs to be converted into *public* and *private* key. You can do that using ```openssl```.
+The tools for this notebook, assume that your public and private key are inside the ```private``` folder.
+If you do not like this behavior you can change the path by exporting the ```CERN_CERTIFICATE_PATH``` environment variable as described [here](https://github.com/CMSTrackerDPG/cernrequests#configuration).
+
+In general you can do the just described steps with:
+
+```bash
+mkdir -p ~/private
+openssl pkcs12 -clcerts -nokeys -in myCertificate.p12 -out ~/private/usercert.pem
+openssl pkcs12 -nocerts -in myCertificate.p12 -out ~/private/userkey.tmp.pem
+openssl rsa -in ~/private/userkey.tmp.pem -out ~/private/userkey.pem
+```
+
+These commands should ask you for the user certificate password. 
+In the simplest case, just enter them twice.
+
+*Note*: Your private key ```userkey.pem``` and public key ```usercert.pem``` have to be **passwordless** for the tools to work.
